dd36eae4aa6207433d830d19fe26c346c029d564
DanOlinski <daniel.guterres@hotmail.com>
2023-03-22 17:39:43 -0600

    add a posted tweet box with mouseover events

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,13 @@
-
-
 //this prevents server from accessing information that hasn't been loaded yet, since that would cause an error
 $(document).ready(function() {
 
-  //jQuery syntax for .addEventListener
+  //jQuery event listener
   $('#tweet-text').on("input", function() {
   //$(this).val() is a useful Jquery function, when nothing is passed into .val() is will return a value if something is passed into .val('something') it will substitute whatever is in front of .val('something') using info from argument (ex: 'something')
     const textVal = $(this).val().length;
     //access .counter class using jQuery. first we access all siblings if there were more siblings we would use [indexNumber] of the location of the sibling we want to access. Next we access all of the children, since we want to narrow down to the child in the second position we use index [1]
     const counter = $(this).siblings().children()[1]
     $(counter).val(140 - textVal);
-
-    console.log($(this).siblings().children())
 
     //this counts the info passed inside a text box, the counter starts at 140, when the value becomes negative change the text to red. if the amount of characters is decreased to 0 or anything above zero remove the red text class
     if (140 - textVal < 0) {
---
+++ public/scripts/tweet-hover-listener.js
@@ -0,0 +1,35 @@
+//the script below prevents server from accessing information that hasn't been loaded yet, since that would cause an error
+$(document).ready(function() {
+
+  //The jQuery modules below add a CSS class that casts a shadow when mouse hovers over the posted tweet box
+  $('.posted-tweet').on("mouseover", function() {
+    $(this).addClass('shade-on-mouseover')
+  });
+  $('.posted-tweet').on("mouseout", function() {
+    $(this).removeClass('shade-on-mouseover')
+  });
+
+  //The jQuery modules below add a CSS class that changes the color of the footer icons(within posted tweet box) when mouse hovers over the icons
+  $('#flag').on("mouseover", function() {
+    $(this).addClass('orange-on-mouseover')
+    console.log(this)
+  });
+  $("#flag").on("mouseout", function() {
+    $(this).removeClass('orange-on-mouseover')
+  });
+  $("#retweet").on("mouseover", function() {
+    $(this).addClass('orange-on-mouseover')
+  });
+  $("#retweet").on("mouseout", function() {
+    $(this).removeClass('orange-on-mouseover')
+  });
+  $("#heart").on("mouseover", function() {
+    $(this).addClass('orange-on-mouseover')
+  });
+  $("#heart").on("mouseout", function() {
+    $(this).removeClass('orange-on-mouseover')
+  });
+});
+
+
+
